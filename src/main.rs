06fb813c8f67918a9ebb1db04d65752591c1a5f9
Ahmed-Zein <ahmedzein3652@gmail.com>
2024-10-24 21:19:22 +0300

    gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use tun_tap::Iface;
+
+fn main() -> Result<(), ()> {
+    let nic = Iface::new("tap-0", tun_tap::Mode::Tun).expect("failed to create a new TUN Devive");
+    let mut buf = [0u8; 1504];
+    nic.recv(&mut buf);
+    Ok(())
 }
